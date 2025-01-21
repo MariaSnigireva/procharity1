117723e99e41f92d449c3f548a0b69bb36e3e1e0
Maria Snigireva <Masch.dark@yandex.ru>
2025-01-21 21:49:22 +0300

    исправления

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,3 +1,11 @@
+import MainPage from '@/Pages/MainPage';
+import LoginPage from '@/Pages/LoginPage';
+import RegisterPage from '@/Pages/RegisterPage';
+
 export const routes = [
-	{ index: true, element: <p>Hello world!</p> }
+	{ index: true, element: <MainPage /> },
+	{ path: '/register', element: <RegisterPage /> },
+	{ path: '/login', element: <LoginPage data={null} error={null} onSubmit={function (email: string, password: string): void {
+		throw new Error('Function not implemented.');
+	} } /> }
 ];
